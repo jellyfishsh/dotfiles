fc79373a8d0f96474e900400ec477931878de54d
jellyfishsh <hikurage@protonmail.com>
2024-12-28 09:16:57 -0800

    changed setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-sudo xbps-install git stow neovim
+
+# I wonder what the best way to separate these would truly be...
+# Probably like:
+
+# Essentials
+sudo xbps-install git stow xtools neovim tmux river ghostty rustup starship fish-shell fastfetch

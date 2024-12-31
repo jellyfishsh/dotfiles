6068c339835e41d76013ad7ed8d624c33601a0fe
Jellyfish <hikurage@protonmail.com>
2024-12-31 10:39:37 -0800

    fixed

---
+++ .config/nvim/init.lua
@@ -0,0 +1,3 @@
+require("config.bindings")
+require("plugins.lazy")
+require("config.options")
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,8 @@
+vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.expandtab = true
+vim.o.nu = true
+vim.o.rnu = true
+vim.o.scr = 10
+vim.o.so = 10
+vim.o.wrap = false

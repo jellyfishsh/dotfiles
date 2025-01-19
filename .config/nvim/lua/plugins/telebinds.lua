77c834f5e4a8ae8e87434f7c815ae7bad77d34fc
Jellyfish <hikurage@protonmail.com>
2025-01-19 12:41:16 -0800

    weird workaround

---
+++ .config/nvim/lua/plugins/telebinds.lua
@@ -0,0 +1,15 @@
+
+-- Telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>pb', builtin.buffers)
+vim.keymap.set('n', '<leader>pg', builtin.live_grep)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+
+-- Setups
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                              , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+}
+
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,3 @@
-
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>pb', builtin.buffers)
-vim.keymap.set('n', '<leader>pg', builtin.live_grep)
-vim.keymap.set('n', '<leader>fh', builtin.help_tags)
-
 -- Setups
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',

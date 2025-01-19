a172828f64434bf09485cee2742f55e3a318bcab
Jellyfish <hikurage@protonmail.com>
2025-01-18 23:58:01 -0800

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 sudo xbps-install -y stow xtools neovim tmux yazi zsh 
 mkdir $HOME/.dotfiles
+cp ./.stow-local-ignore $HOME/.dotfiles/
+stow $HOME/.dotfiles/
+
 cp -r ./.* $HOME/.dotfiles/
 cp -r ./* $HOME/.dotfiles/
-stow $HOME/.dotfiles/
 
 sudo xbps-install -y xorg picom feh bspwm sxhkd wezterm firefox autorandr
 sudo xbps-install -y river wezterm firefox 

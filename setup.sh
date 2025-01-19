12523049f844b10a5b67268495a3dbc3505c2496
Jellyfish <hikurage@protonmail.com>
2025-01-19 14:28:33 -0800

    added starship

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
-sudo xbps-install -y stow xtools neovim tmux yazi zsh 
+
+# CLI Tools
+sudo xbps-install -y stow xtools neovim tmux yazi zsh starship
+
 mkdir $HOME/.dotfiles
 cp ./.stow-local-ignore $HOME/.dotfiles/
 cp -r ./.* $HOME/.dotfiles/
 cp -r ./* $HOME/.dotfiles/
 cd $HOME/.dotfiles && stow . && cd -
 
-sudo xbps-install -y xorg picom feh bspwm sxhkd wezterm firefox autorandr
-sudo xbps-install -y river wezterm firefox 
+sudo xbps-install -y fira-code river wezterm firefox 
 sudo xbps-install -y rustup ghc openjdk21 apache-maven gradle nodejs
 
 mkdir --parents ~/projects/rust

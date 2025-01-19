9694c0326d2d5f2963b494efde05f8d4523f4295
Jellyfish <hikurage@protonmail.com>
2025-01-18 23:47:20 -0800

    deleted Makefile, added and configured setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+sudo xbps-install -y stow xtools neovim tmux yazi zsh 
+mkdir $HOME/.dotfiles
+cp -r ./.* $HOME/.dotfiles/
+cp -r ./* $HOME/.dotfiles/
+stow $HOME/.dotfiles/
+
+sudo xbps-install -y xorg picom feh bspwm sxhkd wezterm firefox autorandr
+sudo xbps-install -y river wezterm firefox 
+sudo xbps-install -y rustup ghc openjdk21 maven gradle nodejs
+
+mkdir --parents ~/projects/rust
+mkdir --parents ~/projects/haskell
+mkdir --parents ~/projects/java
+mkdir --parents ~/projects/javascript
+

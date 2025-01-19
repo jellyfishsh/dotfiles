7f98d69a12ef61bceff31d5ae1f25d1cb80eacf9
Jellyfish <hikurage@protonmail.com>
2025-01-19 00:21:54 -0800

    did an inefficient command for stow. FIX

--- setup.sh
+++ setup.sh
@@ -3,8 +3,7 @@ mkdir $HOME/.dotfiles
 cp ./.stow-local-ignore $HOME/.dotfiles/
 cp -r ./.* $HOME/.dotfiles/
 cp -r ./* $HOME/.dotfiles/
-stow $HOME/.dotfiles/.config
-stow $HOME/.dotfiles/scripts
+cd $HOME/.dotfiles && stow . && cd -
 
 sudo xbps-install -y xorg picom feh bspwm sxhkd wezterm firefox autorandr
 sudo xbps-install -y river wezterm firefox 

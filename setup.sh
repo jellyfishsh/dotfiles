53fd9d7fbaaa4251a533d62016125262af348029
Jellyfish <hikurage@protonmail.com>
2025-01-19 00:11:11 -0800

    changed maven to apache-maven in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ stow $HOME/.dotfiles/
 
 sudo xbps-install -y xorg picom feh bspwm sxhkd wezterm firefox autorandr
 sudo xbps-install -y river wezterm firefox 
-sudo xbps-install -y rustup ghc openjdk21 maven gradle nodejs
+sudo xbps-install -y rustup ghc openjdk21 apache-maven gradle nodejs
 
 mkdir --parents ~/projects/rust
 mkdir --parents ~/projects/haskell

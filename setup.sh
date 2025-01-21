77eaafc66af912838d9dc1c4498165c9bf44946f
Christian Figueroa <cfigueroa@stygio.attlocal.net>
2025-01-20 21:26:49 -0800

    added zsh configuration files

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 
 # CLI Tools
 sudo xbps-install -y stow xtools neovim tmux yazi zsh starship
+sudo xbps-install -Sy void-repo-nonfree
+sudo xbps-install -Sy void-repo-multilib
 
 mkdir $HOME/.dotfiles
 cp ./.stow-local-ignore $HOME/.dotfiles/
 cp -r ./.* $HOME/.dotfiles/
 cp -r ./* $HOME/.dotfiles/
 cd $HOME/.dotfiles && stow . && cd -
+
+
+# Setup zsh
+chsh -s $(which zsh)
 
 sudo xbps-install -y fira-code river wezterm firefox 
 sudo xbps-install -y rustup ghc openjdk21 apache-maven gradle nodejs
@@ -15,3 +21,5 @@ mkdir --parents ~/projects/rust
 mkdir --parents ~/projects/haskell
 mkdir --parents ~/projects/java
 mkdir --parents ~/projects/javascript
+
+exit

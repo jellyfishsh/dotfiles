0cd00239888e73cc6ed5eaa4ebd3adcafb2c7a4e
Jellyfishsh <hikurage@protonmail.com>
2025-02-23 21:45:22 -0800

    Incomplete configuration. Please readjust

--- install.sh
+++ install.sh
@@ -1,11 +1,33 @@
+## NEW CONFIGURATION
+
+#Standard, mandatory packages & licenses
+sudo xbps-install -Sy void-repo-nonfree void-repo-multilib
+sudo xbps-install -y stow xtools zsh unzip
+
+#Setup Dotfiles
+mkdir $HOME/.dotfiles
+cp ./.stow-local-ignore $HOME/.dotfiles/
+cp -r ./.* $HOME/.dotfiles/
+cp -r ./* $HOME/.dotfiles/
+cd $HOME/.dotfiles && stow . && cd -
+
+#Setup FISH
+
+
+
+
+
+
+## OLD CONFIGURATION
+
 # Variables
 $BASE=stow xtools neovim tmux yazi zsh starship unzip
 $LICENSES=void-repo-nonfree void-repo-multilib
-$GUI=font-firacode river wezterm firefox dbus elogind pipewire wireplumber ffmpeg rofi
+$GUI=font-firacode river wezterm firefox dbus elogind pipewire wireplumber ffmpeg dmenu waybar
 $LS=lua-language-server
 $LANGS=rustup openjdk21 apache-maven gradle nodejs
-$TOOLS=tar xz make curl perl gcc binutils pkg-config waybar
-$LIBS=gmp-devel glibc-devel libffi-devel  ncurses-devel ncurses-libtinfo-libs
+$TOOLS=tar xz make curl perl gcc binutils pkg-config
+$LIBS=gmp-devel glibc-devel libffi-devel ncurses-devel ncurses-libtinfo-libs
 
 # CLI Tools
 sudo xbps-install -y $BASE 
@@ -20,10 +42,7 @@ cd $HOME/.dotfiles && stow . && cd -
 
 
 #Projects configuration
-mkdir --parents ~/projects/rust
-mkdir --parents ~/projects/haskell
-mkdir --parents ~/projects/java
-mkdir --parents ~/projects/javascript
+mkdir --parents ~/projects
 
 # Setup zsh
 chsh -s $(which zsh)
@@ -48,7 +67,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 
 ###### EXTRA ######
 
-# setting up custom 
+# setting up custom packages
 mkdir -p $HOME/.local/bin
 mkdir -p $HOME/.local/binpkgs
 
